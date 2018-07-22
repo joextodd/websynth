3d65df2c2c9760f06a3e968392e645e3f8c6c47f
joextodd <joextodd@gmail.com>
2018-07-22 12:19:52 +0100

    Oscillator level control

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
 const h = window.hyperapp.h
 const app = window.hyperapp.app
-const audioContext = new (window.AudioContext || window.webkitAudioContext)()
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 
 const oscillators = ['sine', 'square', 'sawtooth', 'triangle']
-const effects = ['frequency', 'pan', 'phase', 'distortion', 'delay']
+const effects = ['frequency', 'pan', 'filter', 'distortion', 'delay', 'reverb']
 
-const state = { fx: null }
-const initialState = {
-  playing: false, volume: 1.0, frequency: 440,
-  pan: 0.5, phase: 1.0, distortion: 0.0, delay: 0.0
-}
+const initialState = { playing: false }
+const state = { selected: null, pressed: false, fx: effects[0], x: 0, y: 0}
 state.osc = Object.assign(...oscillators.map((o) => ({[o]: initialState})))
-const osc = Object.assign(...oscillators.map(o => ({[o]: audioContext.createOscillator()})))
+
+const osc = Object.assign(...oscillators.map(o => ({[o]: audioCtx.createOscillator()})))
+const volume = Object.assign(...oscillators.map(o => ({[o]: audioCtx.createGain()})))
+const panner = Object.assign(...oscillators.map(o => ({[o]: audioCtx.createStereoPanner()})))
+const filter = Object.assign(...oscillators.map(o => ({[o]: audioCtx.createBiquadFilter()})))
+const delay = Object.assign(...oscillators.map(o => ({[o]: audioCtx.createDelay(100)})))
+const reverb = Object.assign(...oscillators.map(o => ({[o]: audioCtx.createConvolver()})))
 const waves = {
   create: t => {
     osc[t].type = t
-    osc[t].frequency.setValueAtTime(state.osc[t].frequency, audioContext.currentTime)
+    osc[t].frequency.value = 440
+    osc[t].connect(panner[t])
+    panner[t].connect(filter[t])
+    filter[t].connect(delay[t])
+    delay[t].connect(volume[t])
     osc[t].start()
   },
-  start: t => osc[t].connect(audioContext.destination),
-  stop: t => osc[t].disconnect(audioContext.destination),
-  setFrequency: (t, f) => { osc[t].frequency.value = f },
+  start: t => volume[t].connect(audioCtx.destination),
+  stop: t => volume[t].disconnect(audioCtx.destination),
+  setFrequency: (t, f, v) => {
+    volume[t].gain.value = v
+    osc[t].frequency.value = f
+  },
+  setPanning: (t, v) => (panner[t].pan.value = v),
+  setFilter: (t, f, dt) => {
+    filter[t].frequency.value = f
+    filter[t].detune.value = dt
+  },
+  setDelay: (t, v) => (delay[t].delayTime.value = v)
 }
 
-const actions = { setFx: fx => ({ fx }) }
+const actions = {
+  setX: x => ({ x }),
+  setY: y => ({ y }),
+  setPressed: pressed => ({ pressed }),
+  setSelected: selected => ({ selected }),
+  setFx: fx => ({ fx }),
+}
 actions.osc = Object.assign(...oscillators.map(o => ({[o]: {
-  setPlaying: playing => ({ playing }),
-  setVolume: volume => ({ volume }),
-  setFrequency: frequency => ({ frequency }),
+  setPlaying: playing => ({ playing })
 }})))
 
 const view = (s, a) =>
   h('main', {
+    ontouchstart: (e) => a.setPressed(true),
+    ontouchend: (e) => a.setPressed(false),
+    onmousedown: (e) => a.setPressed(true),
+    onmouseup: (e) => a.setPressed(false),
     onmousemove: (e) => {
       let x = e.pageX / window.innerWidth, y = e.pageY / window.innerHeight
-      if (s.fx === 'frequency') {
-        oscillators.forEach(o => {
-          a.osc[o].setVolume(y)
-          a.osc[o].setFrequency(x * 3000)
-          waves.setFrequency(o, x * 3000)
-        })
+      if (s.pressed) {
+        switch (s.fx) {
+          case 'frequency': waves.setFrequency(s.selected, x * 3000, 1.0 - y); break;
+          case 'pan': waves.setPanning(s.selected, x * 2 - 1); break;
+          case 'filter': waves.setFilter(s.selected, x * 3000, y * 100); break;
+          case 'delay': waves.setDelay(s.selected, x * 100); break;
+        }
       }
+      a.setX(e.pageX) && a.setY(e.pageY)
     }
   }, [
     h('div', { class: 'panel' }, oscillators.map((t) =>
       h('button', {
         class: s.osc[t].playing ? `osc ${t} active` : `osc ${t}`,
         oncreate: (e) => waves.create(t),
-        onclick: (e) => s.osc[t].playing ?
+        onclick: (e) => a.setSelected(t) && (s.osc[t].playing ?
           a.osc[t].setPlaying(false) && waves.stop(t) :
-          a.osc[t].setPlaying(true) && waves.start(t)
+          a.osc[t].setPlaying(true) && waves.start(t))
       }, t)
     )),
     h('div', { class: 'panel' }, effects.map((t) =>
@@ -57,7 +83,11 @@ const view = (s, a) =>
         class: s.fx === t ? `fx ${t} active` : `fx ${t}`,
         onclick: (e) => a.setFx(t),
       }, t)
-    ))
+    )),
+    h('div', {
+      class: s.pressed ? 'press' : 'unpress',
+      style: { top: `${s.y - 20}px`, left: `${s.x - 20}px` }
+    }, [])
   ])
 
 app(state, actions, view, document.body)
